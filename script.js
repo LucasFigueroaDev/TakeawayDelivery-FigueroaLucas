8bc2c7aa687efafc85e0b3c625835b3779f1e7dd
LucasFigueroa <lucasafigueroa93@gmail.com>
2024-08-26 23:27:19 -0300

    [M] Declaracion de variables - funciones

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const combos = [
     { id: 4, nombre: "2 Muzzarella", precio: 10500 },
     { id: 5, nombre: "Muzza+especial", precio: 13500 },
     { id: 6, nombre: "1/2 Doc. empanadas", precio: 4000 },
-    { id: 7, nombre: "1 Doc. empanadas, precio", precio: 7800 }
+    { id: 7, nombre: "1 Doc. empanadas", precio: 7800 }
 ];
 
 const extras = [
@@ -21,9 +21,63 @@ const bebidas = [
     { id: 4, nombre: "Lata de cerveza", precio: 1800 }
 ];
 
-let bienvenido = prompt('BIENVENIDO, ¿Te gustaria ver nuestros combos?');
-let envio = 2000;
+// Funcion que calcula el importe total
+function calcularImporteTotal(array) {
+    return array.reduce((total, array) => total + array.precio, 0);
+};
 
+// Funcion que muestra los combos
+function mostrarCombos() {
+    console.log('NUESTROS COMBOS SON:');
+    for (const combo of combos) {
+        console.log(`${combo.id} - ${combo.nombre} $${combo.precio}`);
+    };
+    let pedido = prompt('¿Le gustaria realizar un pedido (ingrese si) o escribe "fin" para terminar?');
+    if (pedido == "si" || pedido == "SI" || pedido == "Si"){
+        hacerPedido();
+    }else {console.log('Tenemos varias ofertas para vos, APROVECHALAS!!!!');
+    }
+};
+
+// Funcion para hacer el pedido
+function hacerPedido() {
+    const combosElegidos = [];
+    while (true) {
+        const eleccion = prompt('Elige un combo (ingresa el número) o escribe "fin" para terminar:');
+        if (eleccion === 'fin' || eleccion === null) {
+            break;
+        };
+        const comboElegido = combos[eleccion - 1];
+        if (comboElegido) {
+            combosElegidos.push(comboElegido);
+            console.log(`Combo agregado: ${comboElegido.nombre}`);
+        } else {
+            console.log('Combo no encontrado, intenta de nuevo.');
+        }
+    };
+    const total = calcularImporteTotal(combosElegidos);
+    alert(`El total de su pedido es: $${total}`);
+
+    const envio = confirm(`¿Desea que se lo enviemos a su domicilio? El envio sale $${envio}`);
+    if (envio == "si" || envio == "SI" || envio == "si quiero"){
+        let domicilio = prompt('¿Cual es su domicilio?');
+        for (const combo of combosElegidos) {
+            alert(`Su pedido es: ${combo.nombre} $${combo.precio}`);
+        }
+        alert(`Se enviara a ${domicilio} y el total es: ${total + envio}`);
+        
+    }else {
+        console.log('Tu pedido estara listo en 20 minutos, podras retirarlo por nuestro local');
+    }
+};
+
+
+
+
+
+
+let bienvenido = prompt('BIENVENIDO, ¿Te gustaria ver nuestros combos (ingresa si) o escribe "fin" para terminar?');
+let envio = 2000;
 if (bienvenido == 'si' || bienvenido == 'Si' || bienvenido == 'SI') {
     mostrarCombos();
 } else console.log('No te olvides de visitar nuestros combos y aprovechar nuestras ofertas');
