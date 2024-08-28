c44e6dccc641ec6cfd0de4b13943f83174d85223
LucasFigueroa <lucasafigueroa93@gmail.com>
2024-08-28 20:46:40 -0300

    [M] Funciones

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-const combos = [
-    { id: 1, nombre: "Lomitos2x1", precio: 9500 },
-    { id: 2, nombre: "Hamburguesas2x1", precio: 8500 },
-    { id: 3, nombre: "Hamburguesa+bebida", precio: 5500 },
-    { id: 4, nombre: "2 Muzzarella", precio: 10500 },
-    { id: 5, nombre: "Muzza+especial", precio: 13500 },
-    { id: 6, nombre: "1/2 Doc. empanadas", precio: 4000 },
-    { id: 7, nombre: "1 Doc. empanadas", precio: 7800 }
-];
-
-const extras = [
-    { id: 1, nombre: "Extra papas fritas", precio: 2000 },
-    { id: 2, nombre: "Pote de mayonesa casera", precio: 800 },
-    { id: 3, nombre: "Pote de salsa picante", precio: 1200 },
-];
-
-const bebidas = [
-    { id: 1, nombre: "Coca Cola 1.5Lt", precio: 1500 },
-    { id: 2, nombre: "Sprite 1.5Lt", precio: 1500 },
-    { id: 3, nombre: "Fanta 1.5Lt", precio: 1500 },
-    { id: 4, nombre: "Lata de cerveza", precio: 1800 }
-];
-
-// Funcion que calcula el importe total
-function calcularImporteTotal(array) {
-    return array.reduce((total, array) => total + array.precio, 0);
-};
-
 // Funcion que muestra los combos
 function mostrarCombos() {
     console.log('NUESTROS COMBOS SON:');
     for (const combo of combos) {
         console.log(`${combo.id} - ${combo.nombre} $${combo.precio}`);
     };
-    let pedido = prompt('¿Le gustaria realizar un pedido (ingrese si) o escribe "fin" para terminar?');
-    if (pedido == "si" || pedido == "SI" || pedido == "Si"){
-        hacerPedido();
-    }else {console.log('Tenemos varias ofertas para vos, APROVECHALAS!!!!');
-    }
 };
 
 // Funcion para hacer el pedido
 function hacerPedido() {
-    const combosElegidos = [];
     while (true) {
         const eleccion = prompt('Elige un combo (ingresa el número) o escribe "fin" para terminar:');
         if (eleccion === 'fin' || eleccion === null) {
@@ -55,33 +21,51 @@ function hacerPedido() {
             console.log('Combo no encontrado, intenta de nuevo.');
         }
     };
-    const total = calcularImporteTotal(combosElegidos);
-    alert(`El total de su pedido es: $${total}`);
-
-    const envio = confirm(`¿Desea que se lo enviemos a su domicilio? El envio sale $${envio}`);
-    if (envio == "si" || envio == "SI" || envio == "si quiero"){
-        let domicilio = prompt('¿Cual es su domicilio?');
-        for (const combo of combosElegidos) {
-            alert(`Su pedido es: ${combo.nombre} $${combo.precio}`);
-        }
-        alert(`Se enviara a ${domicilio} y el total es: ${total + envio}`);
-        
-    }else {
-        console.log('Tu pedido estara listo en 20 minutos, podras retirarlo por nuestro local');
-    }
+    total = calcularImporteTotal(combosElegidos);
+    enviar = confirm(`¿Quiere que se lo enviemos a su domicilio?. El envio cuesta $${envio}`);
+    if (enviar == true){
+        enviarADomicilio();
+    }else alert(`En total es $${total}, estara listo en 20 minutos y podras retirarlo por nuestro local, Muchas gracias.`);
+    
 };
 
+// función enviar a domicilio
+function enviarADomicilio() {
+        domicilio = prompt('¿Cual es su domicilio?');
+        for (const combo of combosElegidos) {
+            console.log(`Su pedido es: ${combo.nombre} $${combo.precio}`);
+        }
+        alert(`Se enviara a ${domicilio} y el total es: ${total + envio}`);
+};
 
+// Funcion que calcula el importe total
+function calcularImporteTotal(array) {
+    return array.reduce((total, array) => total + array.precio, 0);
+};
 
+// Combos 
+const combos = [
+    { id: 1, nombre: "Lomitos2x1", precio: 9500 },
+    { id: 2, nombre: "Hamburguesas2x1", precio: 8500 },
+    { id: 3, nombre: "Hamburguesa+bebida", precio: 5500 },
+    { id: 4, nombre: "2 Muzzarella", precio: 10500 },
+    { id: 5, nombre: "Muzza+especial", precio: 13500 },
+    { id: 6, nombre: "1/2 Doc. empanadas", precio: 4000 },
+    { id: 7, nombre: "1 Doc. empanadas", precio: 7800 }
+];
 
-
-
-let bienvenido = prompt('BIENVENIDO, ¿Te gustaria ver nuestros combos (ingresa si) o escribe "fin" para terminar?');
+alert('Bienvenido a La casa de comida - Lo Más Rico');
+let total = "";
 let envio = 2000;
-if (bienvenido == 'si' || bienvenido == 'Si' || bienvenido == 'SI') {
+let combosElegidos = [];
+let domicilio = "";
+let pedido = confirm('¿Quiere realizar algún pedido?');
+if (pedido == true) {
     mostrarCombos();
-} else console.log('No te olvides de visitar nuestros combos y aprovechar nuestras ofertas');
-
+    hacerPedido();
+} else {
+    console.log('Tenemos varias ofertas para vos, APROVECHALAS!!!!');
+};
 
 
 
