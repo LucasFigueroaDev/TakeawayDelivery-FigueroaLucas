93a3e9c299540f2645f93a26529a020dd4a60044
LucasFigueroa <lucasafigueroa93@gmail.com>
2024-11-20 22:32:21 -0300

    [Pv2] Footer

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,3 @@
-// Funcion que calcula el importe total
-function calcularImporteTotal(array) {
-    return array.reduce((total, array) => total + (array.precio * array.cantidad), 0);
-};
-
 // // Funcion que muestra la cantidad de producto en el carrito
 // function notificacionCarrito(cantidad) {
 //     const cantidadProducto = document.querySelector('.inicio_nav_carrito_notificacion');
@@ -53,10 +48,10 @@ function menuHamburguesa() {
 
 const linksNav = [
     { id: "home", href: "index.html", name: "Home" },
-    { id: "categorias", href: "#categorias", name: "Categorias" },
-    { id: "clientes", href: "#clientes", name: "Clientes" },
-    { id: "nosotros", href: "#nosotros", name: "Nosotros" },
+    { id: "categorias", href: "#categories", name: "Categorias" },
+    { id: "clientes", href: "#customers", name: "Clientes" },
+    { id: "nosotros", href: "#us", name: "Nosotros" },
     { id: "hacerPedido", href: "./html/hacerpedido.html", name: "Hacer pedido" }
 ];
 
-export { calcularImporteTotal, msjAlert, menuHamburguesa, confirmacion, linksNav };
+export { msjAlert, menuHamburguesa, confirmacion, linksNav };
