8d418928d0024bf7f340afa40e1df4197eb21ef1
LucasFigueroa <lucasafigueroa93@gmail.com>
2024-11-20 21:39:18 -0300

    [Pv2] SweetAlert metodos de pago

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { msjAlert, menuHamburguesa, linksNav } from "./utils.js";
+import { msjAlert, menuHamburguesa, linksNav, confirmacion } from "./utils.js";
 // Variable
 let productos;
 let productosCargados = false;
@@ -98,6 +98,7 @@ const addProductCart = (productoId, categoryProducts) => {
         updateLocalStorage();
         msjAlert('Pedido agregado');
         productCartWindow();
+        calculateTotalCart();
     }
 }
 // Creacion del header
@@ -189,7 +190,7 @@ const divProducto = (producto) => {
     btn.setAttribute('id', producto.id);
     btn.textContent = 'Agregar pedido';
 
-    
+
     div.appendChild(img);
     div.appendChild(h3);
     div.appendChild(p);
@@ -235,13 +236,34 @@ const createContainerSelectedProducts = () => {
         boxSelectedProducts.classList.add('selectedProducts-disable');
     });
 
+    const totalCart = document.createElement('p');
+    totalCart.classList.add('selectedProducts_total');
+
+    const buttonFinistOrden = createFinishButton();
+
     boxSelectedProducts.appendChild(boxTitle);
     boxSelectedProducts.appendChild(boxTxt);
     boxSelectedProducts.appendChild(productList);
+    boxSelectedProducts.appendChild(totalCart);
+    boxSelectedProducts.appendChild(buttonFinistOrden);
     boxSelectedProducts.appendChild(emptyCart);
     boxSelectedProducts.appendChild(closeButton);
 
     return boxSelectedProducts;
+};
+// crear boton finalizar compra
+const createFinishButton = () => {
+    const finishOrder = document.createElement('button');
+    finishOrder.classList.add('selectedProducts_finish');
+    finishOrder.textContent = 'Finalizar compra';
+    finishOrder.addEventListener('click', ()=> {
+        if (carrito.length >= 1){
+            formPay();
+        }else {
+            msjAlert('¡El carrito esta vacio!')
+        }
+    });
+    return finishOrder;
 };
 // crear boton vaciar carrito
 const createEmptyCart = () => {
@@ -274,6 +296,8 @@ const updateCartList = () => {
         const productItem = createProductItemElement(item);
         productList.appendChild(productItem);
     });
+
+    calculateTotalCart();
 };
 // crear elemento de artículo de producto
 const createProductItemElement = (item) => {
@@ -322,5 +346,209 @@ const createRemoveButton = (item) => {
 // actualizar almacenamiento local
 const updateLocalStorage = () => {
     localStorage.setItem('carrito', JSON.stringify(carrito));
+};
+
+// Calcular total
+const calculateTotalCart = () => {
+    totalCarrito = carrito.reduce((total, item) => total + item.precio * item.cantidad, 0);
+    const totalCartElement = document.querySelector('.selectedProducts_total');
+    if (totalCartElement) {
+        totalCartElement.textContent = `Total: $${totalCarrito}`;
+    }
+};
+// formulario finalizar orden
+const formPay = () => {
+    const category = document.getElementById('categoryContainer');
+    category.style.display = 'none';
+    const cartProducts = document.getElementById('selectedProducts');
+    cartProducts.classList.replace('selectedProducts', 'selectedProducts-disable');
+        Swal.fire({
+            title: 'Metodo de pago y envio',
+            html: `
+                <label class="inputLabel" for="metodoDePago">Método de pago:</label>
+                <select id="metodoDePago" class="swal2-select input">
+                <option value="tarjeta">Tarjeta de crédito/débito</option>
+                <option value="efectivo">Efectivo</option>
+                </select>
+                <label class="inputLabel" for="metodoDeEntrega">Tipo de entrega:</label>
+                <select id="metodoDeEntrega" class="swal2-select input">
+                <option value="local">Retirar por local</option>
+                <option value="domicilio">Enviar a domicilio</option>
+                </select>`,
+            focusConfirm: false,
+            showCancelButton: true,
+            confirmButtonText: 'Continuar',
+            cancelButtonText: 'Cancelar',
+            customClass: {
+                title: 'title',
+                popup: 'popup',
+                input: 'input',
+                confirmButton: 'sweet-btn',
+                cancelButton: 'sweet-btn'
+            },
+            preConfirm: () => {
+                const metodoDePago = document.getElementById('metodoDePago').value;
+                const metodoDeEntrega = document.getElementById('metodoDeEntrega').value;
+                if (!metodoDePago || !metodoDeEntrega) {
+                    Swal.showValidationMessage('Todos los campos son obligatorios');
+                    return false
+                }
+                return { metodoDeEntrega, metodoDePago }
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                const metodoDeEntrega = result.value.metodoDeEntrega;
+                const metodoDePago = result.value.metodoDePago;
+                if (metodoDePago === 'tarjeta' && metodoDeEntrega === 'local') {
+                    solicitarDatos(metodoDeEntrega, metodoDePago);
+                } else if (metodoDePago === 'tarjeta' && metodoDeEntrega === 'domicilio') {
+                    solicitarDatosDomicilio(metodoDePago, metodoDeEntrega);
+                } else if (metodoDePago === 'efectivo' && metodoDeEntrega === 'domicilio') {
+                    solicitarDatosDomicilio(metodoDePago);
+                } else if (metodoDeEntrega === 'local') {
+                    solicitarDatos(metodoDeEntrega, metodoDePago);
+                }
+            }
+        })
+};
+// Funcion solicitar datos
+const solicitarDatos = (metodoDeEntrega, metodoDePago) => {
+    Swal.fire({
+        title: 'Ingresa tu nombre',
+        html: `
+            <label class="inputLabel" for="nombreCompleto">Tu nombre completo:</label>
+            <input type="text" class="swal2-input" id="nombreCompleto" placeholder="Tu nombre">
+            <label class="inputLabel" for="cel">Tu telefono:</label>
+            <input type="number" class="swal2-input" id="cel" placeholder="Tu telefono">`,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        customClass: {
+            htmlContainer: 'htmlContainer',
+            title: 'title',
+            popup: 'popup',
+            input: 'input',
+            confirmButton: 'sweet-btn',
+            cancelButton: 'sweet-btn'
+        },
+        preConfirm: () => {
+            const nombreCompleto = document.getElementById('nombreCompleto').value;
+            const telefono = document.getElementById('cel').value;
+            if (!nombreCompleto || !telefono) {
+                Swal.showValidationMessage('Debes completar los datos');
+                return false;
+            }
+            return { nombreCompleto, telefono };
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (metodoDePago === 'efectivo') {
+                confirmacion('Pedido registrado. Podras retirarlo en 30 minutos por nuestro local. ¡Muchas Gracias!');
+                carrito = [];
+                updateCartList();
+                calculateTotalCart();
+            } else if (metodoDePago === 'tarjeta' && metodoDeEntrega === 'local') {
+                solicitarDatosTarjeta(metodoDeEntrega);
+            } else if (metodoDePago === 'tarjeta' && metodoDeEntrega === 'domicilio') {
+                solicitarDatosDomicilio(metodoDePago)
+            }
+        }
+    });
+};
+// Funcion solicitar datos de tarjeta
+const solicitarDatosTarjeta = (metodoDeEntrega) => {
+    Swal.fire({
+        title: 'Ingresa los datos de la tarjeta',
+        html: `
+        <label class="inputLabel" for="numeroTarjeta">Número de tarjeta:</label>
+        <input type="number" id="numeroTarjeta" class="swal2-input inputNumber" placeholder="1234 5678 9012 3456">
+        <label class="inputLabel" for="fechaExpiracion">Fecha de expiración:</label>
+        <input type="text" id="fechaExpiracion" class="swal2-input" placeholder="MM/AA">
+        <label class="inputLabel" for="cvv">CVV:</label>
+        <input type="number" id="cvv" class="swal2-input inputNumber" placeholder="123">
+        <label class="inputLabel" for="titular">Titular de la tarjeta:</label>
+        <input type="text" class="swal2-input" id="titular" placeholder="Titular de la tarjeta">
+        <label class="inputLabel" for="dni">DNI:</label>
+        <input type="number" class="swal2-input" id="dni" placeholder="Tu dni">`,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: 'Pagar',
+        customClass: {
+            htmlContainer: 'htmlContainer',
+            title: 'title',
+            popup: 'popup',
+            input: 'input',
+            confirmButton: 'sweet-btn',
+            cancelButton: 'sweet-btn'
+        },
+        preConfirm: () => {
+            const numeroTarjeta = document.getElementById('numeroTarjeta').value;
+            const fechaExpiracion = document.getElementById('fechaExpiracion').value;
+            const cvv = document.getElementById('cvv').value;
+            const titular = document.getElementById('titular').value;
+            const dni = document.getElementById('dni').value;
+            if (!numeroTarjeta || !fechaExpiracion || !cvv || !titular || !dni) {
+                Swal.showValidationMessage('Debes completar todos los campos de la tarjeta');
+                return false;
+            } else if (numeroTarjeta === Number && dni === Number) {
+                Swal.showValidationMessage('Debes ingresar número de tarjeta valido');
+            }
+            return { numeroTarjeta, fechaExpiracion, cvv, titular, dni };
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (metodoDeEntrega === 'domicilio') {
+                confirmacion(`¡Pago aprobado con exitos! Su pedido se entregara a tu domicilio. ¡Muchas Gracias!`);
+            } else if (metodoDeEntrega === 'local') {
+                confirmacion(`¡Pago aprobado con exitos! Podras retirarlo en 30 minutos por nuestro local. ¡Muchas Gracias!`);
+            }
+        }
+        carrito = [];
+        updateCartList();
+        calculateTotalCart();
+    });
+};
+// Funcion solicitar datos del domicilio
+const solicitarDatosDomicilio = (metodoDePago, metodoDeEntrega) => {
+    Swal.fire({
+        title: 'Ingresa los datos de envío',
+        html: `
+                <label class="inputLabel" for="nombre">Tu nombre:</label>
+                <input type="text" id="nombre" class="swal2-input" placeholder="Tu nombre">
+                <label class="inputLabel" for="direccion">Dirección:</label>
+                <input type="text" id="direccion" class="swal2-input" placeholder="Domicilio de entrega">
+                <label class="inputLabel" for="telefono">Teléfono:</label>
+                <input type="number" id="telefono" class="swal2-input" placeholder="Tu telefono">`,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar Envío',
+        customClass: {
+            htmlContainer: 'htmlContainer',
+            title: 'title',
+            popup: 'popup',
+            input: 'input-dom',
+            confirmButton: 'sweet-btn',
+            cancelButton: 'sweet-btn'
+        },
+        preConfirm: () => {
+            const nombre = document.getElementById('nombre').value;
+            let direccion = document.getElementById('direccion').value;
+            const telefono = document.getElementById('telefono').value;
+            if (!nombre || !direccion || !telefono) {
+                Swal.showValidationMessage('Debes completar todos los campos de envío');
+                return false;
+            }
+            return { nombre, direccion, telefono };
+        }
+    }).then((result) => {
+        if (metodoDePago === 'tarjeta') {
+            solicitarDatosTarjeta(metodoDeEntrega);
+        } else {
+            confirmacion(`¡Envío registrado! Su pedido se entregará en ${result.value.direccion}. Podrás abonarlo cuando llegue el cadete. ¡Gracias por la compra!`);
+            carrito = [];
+            updateCartList();
+            calculateTotalCart();
+        }
+    });
 };
 // menuHamburguesa();
